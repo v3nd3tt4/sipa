b22a427d7c071d4d5790fc971dcca2e7b949deed
v3nd3tt4 <pilopaokta@gmail.com>
2020-03-23 19:05:17 +0700

    inisiasi 2

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Dashboard';
+        $this->load->view('_layout_sifa/header', $data);
+        $this->load->view('_layout_sifa/sidebar', $data);
+        $this->load->view('_layout_sifa/topbar', $data);
+        $this->load->view('dashboard/index', $data);
+        $this->load->view('_layout_sifa/footer');
+    }
+}
